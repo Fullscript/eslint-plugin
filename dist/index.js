921643683e8f08982e9ec855901c140c584ffb35
Moro-Code <omar@omarnasr.ca>
2023-01-24 20:44:42 -0500

    feat: create a rule to prevent jest from being used in production code

--- dist/index.js
+++ dist/index.js
@@ -12,6 +12,7 @@ const _gqlRules = require("./gqlRules");
 const _noRenamedTranslationImport = require("./noRenamedTranslationImport");
 const _noUnawaitedSkeletons = require("./noUnawaitedSkeletons");
 const _oneTranslationImport = require("./oneTranslationImport");
+const _noJestInProduction = require("./noJestInProduction");
 const rules = {
     "one-translation-import-per-file": _oneTranslationImport.oneTranslationImport,
     "no-renamed-translation-import": _noRenamedTranslationImport.noRenamedTranslationImport,
@@ -19,5 +20,6 @@ const rules = {
     "gql-operation-name": _gqlRules.gqlOperationName,
     "cross-reference": _crossReference.crossReference,
     "circular-dependency": _circularDependency.circularDependency,
-    "no-unawaited-skeletons": _noUnawaitedSkeletons.noUnawaitedSkeletons
+    "no-unawaited-skeletons": _noUnawaitedSkeletons.noUnawaitedSkeletons,
+    "no-jest-in-production": _noJestInProduction.noJestInProduction
 };
---
+++ src/noJestInProduction/noJestInProduction.js
@@ -0,0 +1,39 @@
+const meta = {
+  type: "problem",
+  docs: {
+    description: "Prevents jest from being used in production code",
+    category: "no-jest-in-production",
+    recommended: false,
+  },
+  fixable: null,
+  schema: [],
+};
+
+const create = context => {
+  // split the filepath into an array
+  const filePath = context.getFilename().split("/");
+  // the last entry will always be the file name
+  const fileName = filePath.at(-1);
+
+  // Spec files are allowed to have jest, so we skip them
+  const isNotSpec = () => {
+    return fileName && !fileName.includes(".spec.");
+  };
+
+  const isNotValid = node => {
+    return isNotSpec() && node.name === "jest";
+  };
+
+  return {
+    Identifier(node) {
+      if (isNotValid(node)) {
+        context.report({
+          node,
+          message: "Jest should not be used in production code",
+        });
+      }
+    },
+  };
+};
+
+export { meta, create };
