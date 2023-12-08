bf7104625bddca0634ef2984f921afed286e3f0b
asukakuwahara <kuwahara.mee@gmail.com>
2023-12-08 10:30:15 -0500

    early return for ignore list

--- src/gqlRules/gqlNoManualHookDeclaration/gqlNoManualHookDeclaration.js
+++ src/gqlRules/gqlNoManualHookDeclaration/gqlNoManualHookDeclaration.js
@@ -30,12 +30,15 @@ const reactHookPrefix = new RegExp("use\\w+");
 
 const create = context => {
   const isGqlObjectFile = isGqlFile(context);
-  const { namespaceIgnoreList } = context.options[0];
+  const ignoreList = context?.options?.[0]?.namespaceIgnoreList ?? [];
 
   const isInIgnoreList = () => {
+    if (!ignoreList?.length) return false;
     const pathToFile = relativePathToFile(context);
-    return namespaceIgnoreList.some(ignoredNamespace => pathToFile.startsWith(ignoredNamespace));
+    return ignoreList.some(ignoredNamespace => pathToFile.startsWith(ignoredNamespace));
   };
+
+  if (isInIgnoreList() || !isGqlObjectFile) return {}; // return early before going through nodes
 
   const isHookDeclaredManually = (node, name) => {
     if (reactHookPrefix.test(name)) {
@@ -47,8 +50,8 @@ const create = context => {
   };
 
   const ExportNamedDeclaration = node => {
-    // not a gql file or no value exported
-    if (isInIgnoreList() || !isGqlObjectFile || node.exportKind !== "value") return;
+    // no values in export statement
+    if (node.exportKind !== "value") return;
 
     // When the variable is directly exported like so export const usePatientQuery =...
     if (node.declaration && node.declaration.declarations) {
