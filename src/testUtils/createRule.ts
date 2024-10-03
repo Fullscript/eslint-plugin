8cdcb7e0a87f5858695761243675facd45fe94a7
yamato.murakami <yamato.murakami@fullscript.com>
2024-10-03 09:23:41 -0400

    fix url

--- src/testUtils/createRule.ts
+++ src/testUtils/createRule.ts
@@ -1,7 +1,7 @@
 import { ESLintUtils } from "@typescript-eslint/utils";
 
 const createRule = ESLintUtils.RuleCreator(
-  () => "https://github.com/pipopotamasu/eslint-plugin-no-implicit-any"
+  () => "https://github.com/Fullscript/eslint-plugin"
 );
 
 export { createRule };
