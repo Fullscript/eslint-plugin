a34021da42d5ae3ffcd8f0945448ffa6728fa624
Mike Presman <michael.presman@fullscript.com>
2024-12-06 14:50:07 -0500

    changes, time to test

--- src/noConditionalsInReturnStatements/noConditionalsInReturnStatements.ts
+++ src/noConditionalsInReturnStatements/noConditionalsInReturnStatements.ts
@@ -10,36 +10,78 @@ export const meta = {
     {
       type: "object",
       properties: {
-        allowedConditionals: { type: "array", ignoredFiles: { type: "string" } },
+        allowedConditionals: { type: "array", ignoredFilePaths: { type: "string" } },
       },
     },
   ],
   defaultOptions: {
-    ignoredFiles: [],
+    ignoredFilePaths: [],
   },
 };
 
 
-const searchForConditionalsInReturnStatements = (context): Array<string> => {
-  return [];
+interface ReturnStatementWithConditionalExpression {
+  status: boolean;
+  conditionals: {
+    position: number
+    node: Node;
+  }[]
+}
+
+export const searchForConditionalsInReturnStatements = (filePath: string, sourceCode: string | any): Array<ReturnStatementWithConditionalExpression> => {
+  const allReturnStatements = sourceCode.findNodes(ast => ast.type === "ReturnStatement");
+
+  return allReturnStatements.map(returnStatement => {
+    const conditionals = returnStatement.findNodes(ast => ast.type === "ConditionalExpression");
+
+    if (conditionals.length === 0) {
+      return {
+        status: false,
+        conditionals: [],
+      }
+    }
+
+    const conditionalsInAGivenReturnStatement = conditionals.map(conditional => {
+      return {
+        position: conditional.range[0],
+        node: conditional,
+      };
+    });
+
+    return {
+      status: true,
+      conditionals: conditionalsInAGivenReturnStatement,
+    }
+
+  });
 };
 
 
 // Note: Context is the current context
 export const create = context => {
-  const { ignoredFiles } = context.options?.[0] || {};
-  const filename = context.getFilename();
+  const { ignoredFilePaths } = context.options?.[0] || {};
+  const filePath = context.physicalFilename;
 
-  if (ignoredFiles.includes(filename)) {
+  if (ignoredFilePaths.includes(filePath)) {
     return {};
   }
 
-  const result = searchForConditionalsInReturnStatements(context);
-
+  const sourceCode = context.sourceCode();
+  const result = searchForConditionalsInReturnStatements(filePath, sourceCode);
 
   return {
-    ReturnStatement(node) {
-      console.log(node);
-    },
+    ReturnStatement: (node) => {
+      result.forEach(returnStatement => {
+        if (returnStatement.status) {
+          returnStatement.conditionals?.forEach(conditional => {
+            context.report({
+              node: conditional.node,
+              message: "Conditional expression found in return statement at position ${conditional.position}",
+            });
+          })
+        }
+      })
+    }
   };
 };
+
